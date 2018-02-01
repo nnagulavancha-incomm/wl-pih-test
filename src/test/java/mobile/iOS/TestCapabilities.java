f0fefe62a5cd1130f437489f84093eb16e0f8cb6
Naresh Kumar Nagulavancha <nnagulavancha@incomm.com>
2018-02-01 16:27:51 -0500

    os version

--- src/test/java/mobile/iOS/TestCapabilities.java
+++ src/test/java/mobile/iOS/TestCapabilities.java
@@ -12,7 +12,7 @@ package mobile.iOS;
 public class TestCapabilities {
     public static final String APP_URL = "/Users/vagrant/customBuildDir/wl_pih.app";
     public static final String PLATFORM_NAME = "iOS";
-    public static final String PLATFORM_VERSION = "11.2";
+    public static final String PLATFORM_VERSION = "11.0";
     public static final String AUTOMATION_NAME = "XCUITest";
     public static final String DEVICE_NAME = "iPhone 6s Plus";
     public static final String END_POINT = "http://localhost:4723/wd/hub";
